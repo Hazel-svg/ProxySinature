8b83902d1596b16fcb4e570c4d8d1ff1a499ae45
hanmeimei <hanmeimeizhy@gmail.com>
2019-12-17 21:43:11 +0800

    TT

--- myUI/myAcceptAgent.py
+++ myUI/myAcceptAgent.py
@@ -8,12 +8,16 @@ class MyAcceptAgent(Ui_AcceptAgent):
     def __init__(self, view, passwd_arg):
         super().setupUi(view)
         self.btn_acceptagent.clicked.connect(self._on_btn_ok_clicked)
+        self.btn_rejectagent.clicked.connect(self.close)
 
         # 成员变量获取新口令, 使用list 为引用传值
         self.passwd_arg = passwd_arg
 
     def _on_btn_ok_clicked(self):
         self._modifyPasswd()
+   
+
+    
 
 
     def _modifyPasswd(self):
